3958a1425cf980f2acffd0761ee5fff8c2101757
sindhu-ku <kumaran.sindhujha@gmail.com>
2024-06-05 00:23:33 -0700

    update test script

--- test_runsdb.py
+++ test_runsdb.py
@@ -1,8 +1,8 @@
 from Query2x2.LRS.LRS_query import LRS_blob_maker
 from Query2x2.SC.SC_query import SC_blob_maker
 
+subrun_dict = LRS_blob_maker(run=20, get_subrun_dict=True)
+#print(subrun_dict)
+#SC_blob_maker(measurement="runsdb", run=20, subrun_dict=subrun_dict) # this takes too much time because of too many queries (~1 min)
 
-subruns = LRS_blob_maker(run=20)
-
-for subrun, times in subruns.items():
-    SC_blob_maker(start=times['start_time'], end=times['end_time'], measurement="runsdb", run=20, subrun=subrun)
+SC_blob_maker(start_time="2024-06-03 02:08:08", end_time="2024-06-03, 08:24:12", measurement="runsdb", run=20) #for the same start and end takes 5 seconds
