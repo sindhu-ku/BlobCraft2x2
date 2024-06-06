8adc266308487f78150a63f4ca0519e984c6200e
sindhu-ku <kumaran.sindhujha@gmail.com>
2024-06-06 14:10:51 -0700

    BlobCraft2x2 it is

--- test_runsdb.py
+++ test_runsdb.py
@@ -1,9 +1,9 @@
-from Query2x2.LRS.LRS_query import LRS_blob_maker
-from Query2x2.SC.SC_query import SC_blob_maker
+from BlobCraft2x2.LRS.LRS_query import LRS_blob_maker
+from BlobCraft2x2.SC.SC_query import SC_blob_maker
 
 subrun_dict = LRS_blob_maker(run=20, get_subrun_dict=True) #gets subrun_dict but only dumps summary info
 
-LRS_blob_maker(run=20, get_subrun_dict=False, dump_all_data=True)   #dumps all tables in LRS DB
+LRS_blob_maker(run=20, dump_all_data=True)   #dumps all tables in LRS DB
 
 SC_blob_maker(measurement_name="runsdb", run_number=20, subrun_dict=subrun_dict) #only dumps summary SC data of LRS subruns
 
