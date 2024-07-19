ee9ad05c3daea26c1ac16712e5f15e05a3de91ab
Matt Kramer <mjkramer@users.noreply.github.com>
2024-07-19 10:05:48 -0700

    Hack: Add --sql-format to CRS_blob_maker
    For easy conversion (via Pandas) into an sqlite DB. We've gone full
    circle! TODO: Write CRS_build.py to go directly from data files to an
    sqlite DB, then make CRS_query (and build_subrun_dict) use that, and get
    rid of this --sql-format.

--- src/BlobCraft2x2/CRS/CRS_query.py
+++ src/BlobCraft2x2/CRS/CRS_query.py
@@ -19,13 +19,16 @@ def get_config_data(h5path):
     return tarfile.open(fileobj=fileobj)
 
 
-def CRS_blob_maker(run):
+def CRS_blob_maker(run, sql_format=False):
     print(f"\n----------------------------------------Fetching CRS data for the run {run}----------------------------------------")
 
     config = load_config("config/CRS_parameters.yaml")
     data_dir = config['data_dir']
 
-    output = {}
+    if sql_format:
+        output = []
+    else:
+        output = {}
 
     files = Path(data_dir).rglob(f'binary-{run:07d}-*.h5')
 
@@ -39,17 +42,27 @@ def CRS_blob_maker(run):
             end_time = f['meta'].attrs['modified']
             msg_rate = len(f['msgs']) / (end_time - start_time)
 
+            info['subrun'] = subrun
+            info['morcs_run_nr'] = run
+            info['start_time_unix'] = start_time
+            info['end_time_unix'] = end_time
             info['start_time'] = unix_to_iso(start_time);
             info['end_time'] = unix_to_iso(end_time);
             info['msg_rate'] = msg_rate
 
-        output[subrun] = info
+        if sql_format:
+            output.append(info)
+        else:
+            output[subrun] = info
 
     start_subrun, end_subrun = 1, len(output)
-    start_str = output[start_subrun]['start_time']
-    end_str = output[end_subrun]['end_time']
+    start_str = output[0]['start_time'] if sql_format else output[start_subrun]['start_time']
+    end_str = output[-1]['end_time'] if sql_format else output[end_subrun]['end_time']
 
     fname = f'CRS_all_ucondb_measurements_run-{run}-{start_str}_{end_str}'
+    if sql_format:
+        fname += '.SQL'
+
     dump(output, fname)
     return output
 
@@ -57,9 +70,10 @@ def CRS_blob_maker(run):
 def main():
     parser = argparse.ArgumentParser(description="Query SQLite database and dump data to JSON file.")
     parser.add_argument("--run", type=int, required=True, help="Run number")
+    parser.add_argument('--sql-format', action='store_true')
     args = parser.parse_args()
 
-    CRS_blob_maker(args.run)
+    CRS_blob_maker(args.run, sql_format=args.sql_format)
 
 
 if __name__ == "__main__":
