687cc0aed8296740dc13c227a7f8e22e373947e2
Matt Kramer <mjkramer@users.noreply.github.com>
2024-11-02 11:08:41 -0700

    Get timezone from new Global_parameters.yaml

--- src/BlobCraft2x2/__init__.py
+++ src/BlobCraft2x2/__init__.py
@@ -3,14 +3,15 @@ from zoneinfo import ZoneInfo
 import yaml
 
 
-local_tz = ZoneInfo('America/Chicago')
-
 def load_config(config_file):
     with open(config_file, 'r') as f:
         return yaml.safe_load(f)
 
+Global_config = load_config('config/Global_parameters.yaml')
 CRS_config = load_config('config/CRS_parameters.yaml')
 LRS_config = load_config('config/LRS_parameters.yaml')
 Mx2_config = load_config('config/Mx2_parameters.yaml')
 SC_config = load_config('config/SC_parameters.yaml')
 IFbeam_config = load_config('config/IFbeam_parameters.yaml')
+
+local_tz = ZoneInfo(Global_config['timezone'])
